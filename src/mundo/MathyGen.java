7f3e6595edab47c4df0cea4bf8d0fb33359f33c0
nerzhul250 <gamersav1822@gmail.com>
2017-10-25 13:52:53 -0500

    pushing
    batch

--- src/mundo/MathyGen.java
+++ src/mundo/MathyGen.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class MathyGen {
 
-	public final static int ANCHOPLANO = 1000;
+	public final static int ANCHOPLANO =600;
 	public final static int LARGOPLANO =600;
 	private Punto primerPunto;
 	private Funcion raizFuncion;
