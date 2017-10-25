41e8c876cec2d1b79a2d13b33dd6e37753d0148f
StevenMaMei <33093117+StevenMaMei@users.noreply.github.com>
2017-10-25 13:43:04 -0500

    antes de arango

--- src/mundo/MathyGen.java
+++ src/mundo/MathyGen.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 public class MathyGen {
 
 	public final static int ANCHOPLANO = 1000;
-	public final static int LARGOPLANO =600;
+	public final static int LARGOPLANO =1000;
 	private Punto primerPunto;
 	private Funcion raizFuncion;
 	private ArrayList<Region> listaRegiones;
