fb8687851b2ac9a1df6039e4877b5c0ccd38a488
nerzhul250 <gamersav1822@gmail.com>
2017-11-21 10:23:35 -0500

    hi

---
+++ test/test/TestNumeroPrimo.java
@@ -0,0 +1,20 @@
+package test;
+
+import static org.junit.Assert.*;
+import mundo.NumeroPrimo;
+
+import org.junit.Test;
+
+public class TestNumeroPrimo {
+	private NumeroPrimo numeroPrimo;
+	private void setUpEscenario1(){
+		numeroPrimo=null;
+	}
+	@Test
+	public void testInicializacionNumeroPrimo() {
+		setUpEscenario1();
+		numeroPrimo=new NumeroPrimo("7",4);
+		assertEquals("7",numeroPrimo.getNumero().toString());
+	}
+
+}
